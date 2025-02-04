8cf870fc414c1e5dbb459e45b8341e80b8d16507
Mohanaprabhu.R <mohan@timeless.co>
2025-02-04 15:03:05 +0530

    Color Changed

--- src/components/show/category.tsx
+++ src/components/show/category.tsx
@@ -111,7 +111,7 @@ const GenreCarouselItem: React.FC<GenreCarouselItemProps> = ({
                                   onChange={(e) =>
                                     handleSeasonChange(show.id, e.target.value)
                                   }
-                                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                  className="mt-1 block w-full text-black rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                                 >
                                   {seasons.map((season) => (
                                     <option key={season} value={season}>
